e2507f83f4a3383ad85f5eb40023f9a7695e0c02
Eirik Lygre <eirik.lygre@gmail.com>
2011-02-16 22:12:36 +0100

    Clean branch gh-pages

--- pldoc/ikb.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-setlocal
-cd iknowbase
-call ikb
-endlocal
--- pldoc/pldoc_example.bat
+++
@@ -1,2 +0,0 @@
-call pldoc.bat -doctitle 'Sample Application' -overview src/samples/overview1.html -d target/SampleApplicationDoc src/samples/sample*.sql
-PAUSE
