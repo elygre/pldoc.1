20629251cf231cec6777c11db221c2fdf1d175e2
Eirik Lygre <eirik.lygre@gmail.com>
2011-02-16 20:34:38 +0100

    Added more and better type samples.

--- samples/sampletype.sql
+++ samples/sampletype.sql
@@ -1,32 +1,56 @@
--- Prompt Type PORTLET_PREFS_PARAM_TYPE;
-CREATE OR REPLACE TYPE PORTLET_PREFS_PARAM_TYPE AS OBJECT (
-      value_id           VARCHAR2 (50),
-      value_type         VARCHAR2 (2),
-      VALUE              VARCHAR2 (1000),
-      value_cond         VARCHAR2 (20),
-      dyn_pref_id        NUMBER,
-      value_guid         VARCHAR2 (50),
-      attribute_guid     VARCHAR2 (50),
-      param_type         VARCHAR2 (1),
-      dynamic_param_id   NUMBER,
-      sql_condition      VARCHAR2(30000),
-      
-      /**
-       * The dynamic attribute id, very strange stuff.
-       * Remember that this can be used time and time again.
-       */
-      dyn_attribute_id   varchar2(40),
-      dyn_param_condition varchar2(10),
-      datatype           varchar2(10),
-      alias_name         varchar2(100),
-      attribute_type     varchar2(100),
-      where_clause       varchar2(200),
-      guid_values        varchar2(10000)
+CREATE OR REPLACE TYPE object_type AS OBJECT (
+	/** The ID of the object */
+    value_id           	NUMBER
+	
+	/** The name of the object */
+	value_name			VARCHAR2 (2),
+	
+	/** A guid, for whatever purpose */
+	value_guid			RAW(32),
+	
+	/**
+	 * The date the object was last imagined.
+	 *
+	 * This is a strange property, but still 
+	 * very useful. It contains the date of
+	 * the last time anyone imagined having
+	 * the date of the last time anyone
+	 * imagined having the date of the last
+	 * time... etc ad infinitum
+	*/
+	value_date			date,
+
+	/** A string representation */
+	member function to_string return varchar2 cascade,
+	
+	/** a map member */
+	map member function get_ident return varchar2
 )
 /
 
 /**
  * This is a table of the real stuff
  */
-CREATE OR REPLACE TYPE PORTLET_PREFS_PARAM_TYPES AS TABLE OF PORTLET_PREFS_PARAM_TYPE
+CREATE OR REPLACE TYPE table_type AS TABLE OF PORTLET_PREFS_PARAM_TYPE
+/
+
+/**
+ * This is a table of the real stuff
+ */
+CREATE OR REPLACE TYPE varray_type AS VARRAY(100) OF PORTLET_PREFS_PARAM_TYPE
+/
+
+create type finaltype as object (
+	id number
+) final
+/
+
+create type supertype as object (
+	id number
+) not final
+/
+
+create type subtype under supertype (
+    extra clob
+) final
 /
