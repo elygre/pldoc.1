5b74ba4c9788ae54aea8bfec8223d149ff0dfefb
Eirik Lygre <eirik.lygre@gmail.com>
2011-02-17 00:44:44 +0100

    Support for CASE-expressions in declarations

--- pldoc/pldoc_example.bat
+++ pldoc/pldoc_example.bat
@@ -1,2 +1,2 @@
-call pldoc.bat -doctitle 'Sample Application' -overview src/samples/overview1.html -d target/SampleApplicationDoc src/samples/sample*.sql
+call pldoc.bat -doctitle 'Sample Application' -overview src/samples/overview1.html -d target/SampleApplicationDoc src/samples/*.sql
 PAUSE
