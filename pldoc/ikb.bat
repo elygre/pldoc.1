13983b63dbfe4166bc47ab9a7428519dfc882c5b
Eirik Lygre <eirik.lygre@gmail.com>
2011-03-08 18:57:18 +0100

    Deleted iKnowBase-specific file.

--- pldoc/ikb.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-setlocal
-cd iknowbase
-call ../pldoc.bat -doctitle 'iKnowBase' -d iKnowBase e:/iknowbase_trunk/Database/source/dbapi/packages/*.pks 
-rem call ../pldoc.bat -doctitle 'iKnowBase' -d iKnowBase e:/iknowbase_trunk/Database/source/providers/common/ikb_htf.pks
-rem call ikb
-endlocal
